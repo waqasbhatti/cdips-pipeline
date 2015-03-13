ee7e7e1eea31bca24ebbd86345fd89a459242d4e
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2015-03-13 19:14:25 -0400

    added skeletons for calibration and ISM python modules

---
+++ calibration.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+'''
+calibration.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - March 2015
+
+This contains functions to calibrate the HATPI images and make reduced frames.
+
+'''
+
