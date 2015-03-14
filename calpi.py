59fcc2dd14cf7311dde4fa6ad42c42eb037cdbbe
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2015-03-14 00:46:59 -0400

    calibration.py renamed to calpi.py

--- calpi.py
+++ calpi.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 '''
-calibration.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - March 2015
+calpi.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - March 2015
 
 This contains functions to calibrate the HATPI images and make reduced frames.
 
 '''
-
