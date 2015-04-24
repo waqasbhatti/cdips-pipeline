3b7deff93ea994cb43a2b85209f308ed29abf323
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2015-04-24 12:36:56 -0400

    a bit more fleshed skeleton of the imagesubphot module

--- imagesubphot.py
+++ imagesubphot.py
@@ -45,6 +45,10 @@ import imageutils
 from imageutils import get_header_keyword
 
 
+#################
+## DEFINITIONS ##
+#################
+
 # set this to show extra info
 DEBUG = False
 
@@ -65,95 +69,118 @@ ZEROPOINTS = {5:17.11,
 # used to get the station ID, frame number, and CCD number from a FITS filename
 FRAMEREGEX = re.compile(r'(\d{1})\-(\d{6}\w{0,1})_(\d{1})')
 
-# command string to do a 2massread for a specified FOV
-TWOMASSREADCMD = ("2massread -r {ra:f} -d {dec:f} -s {boxlen:f} "
-                  "--cat {catalogpath} -mr {brightrmag:f} -Mr {faintrmag:f} "
-                  "--xieta-coords {ra:f} {dec:f} -o {outfile}")
 
-# command string to do a 2massread for a specified FOV
-UCAC4READCMD = ("ucac4read -r {ra:f} -d {dec:f} -s {boxlen:f} "
-                  "--cat {catalogpath} -mr {brightrmag:f} -Mr {faintrmag:f} "
-                  "-o {outfile}")
+##################################
+## ASTROMETRIC REFERENCE FRAMES ##
+##################################
 
-# locations of catalogs
-CATALOGS = {
-    '2MASS':{'cmd':TWOMASSREADCMD,
-             'path':'/nfs/lcohpsrv1/ar0/P/HP0/CAT/2MASS/2MASS_JH_AP/data'},
-    'UCAC4':{'cmd':UCAC4READCMD,
-             'path':'/nfs/lcohpsrv1/ar0/P/HP0/CAT/UCAC4'}
-    }
+def select_astrometric_reference_frame(photdir):
+    '''
+    This picks an astrometric reference frame.
 
-# command string to run fistar
-# parameters:
-# {frame}
-# {extractedlist}
-# {ccdgain}
-# {zeropoint}
-# {exptime}
-# {fluxthreshold}
-# {ccdsection}
-FISTARCMD = ("{fistarexec} -i {frame} -o {extractedlist} "
-             "--model elliptic --iterations symmetric=4,general=2 "
-             "--algorithm uplink --format id,x,y,bg,amp,s,d,k,flux,s/n "
-             "-g {ccdgain} --mag-flux {zeropoint},{exptime} --sort flux "
-             "--flux-threshold {fluxthreshold} --section {ccdsection} "
-             "--comment")
+    We're looking for:
 
-# command string to run transformation from RA/Dec in 2MASS catalogs to X/Y in
-# FITs image to eventually run photometry at those locations using the
-# transformations noted in each frame's .wcs file. we do the transform, then
-# remove any objects outside the [0:2048, 0:2048] box for the CCD. we then use
-# the resulting source list as input to fiphot. the parameters are:
+    - highest median S (smallest FWHM)
+    - lowest median background
+    - smallest Z
 
-# {transformer}:       'anrd2xy' or similar, coord -> pix converter executable
-# {framewcsfile}:      WCS transformation file associated with frame
-# {catalogsourcelist}: 2MASS catalog file associated with camera FOV
-# {outfile}:           temporary output file
-TRANSFORMCMD = ("{transformer} -w {framewcsfile} "
-                "-o {outputfile} "
-                "-c 2,3 "
-                "{catalogsourcelist} ")
-
-# fiphot command string to run fiphot. requires a sourcelist obtained from
-# running TRANSFORMCMD and removing objects outside the CCD. the parameters are:
-# FIXME: WTF is single-background and why is it 3?
-
-# {fits}:         name of input FITS frame
-# {sourcelist}:   name of the source list file
-# {zeropoint}:    zeropoint magnitude from ZEROPOINTS above
-# {xycols}:       comma-separated 1-indexed column numbers of x and y coords
-# {ccdgain}:      gain of the CCD
-# {ccdexptime}:   exposure time of the CCD in seconds
-# {aperturelist}: aperture list in the following format (all units are pixels):
-#                 aper1rad:sky1inner:sky1rad,...,aperNrad,skyNinner,skyNrad
-# {fitsbase}:     the FITS base filename without any extensions
-#                 e.g. for 1-377741e_5.fits, this is 1-377741e_5
-#                 (used for later collection of fiphot files into an LC)
-# {outfile}:      name of the output .phot file (binary format)
-# the imagesub fiphot aperture string is:
-# --apertures '2.5:7.0:6.0,2.95:7.0:6.0,3.35:7.0:6.0,3.95:7.0:6.0'
-# but we should use 3 apertures, so this will become:
-# --apertures '2.95:7.0:6.0,3.35:7.0:6.0,3.95:7.0:6.0'
-
-FIPHOTCMD = ("fiphot --input {fits} --input-list {sourcelist} "
-             "--col-id 1 --col-xy {xycols} --gain {ccdgain:f} "
-             "--mag-flux {zeropoint:f},{ccdexptime:f} "
-             "--apertures {aperturelist} "
-             "--sky-fit 'mode,sigma=3,iterations=2' --disjoint-radius 2 "
-             "--serial {fitsbase} "
-             "--format 'ISXY,BbMms' --nan-string 'NaN' "
-             "--aperture-mask-ignore 'saturated' --comment '--comment' "
-             "--single-background 3 {binaryout} --output {outfile} -k")
+    TODO:
 
 
-## list of tasks
-
-# 1. run fistar on everything, run astrometry on everything
-
-# 2. find a good astrometry reference (sharp stars, low zenith distance, new
-    # moon)
-
-# 3. run grmatch to get shifts between each frame
+    '''
 
 
+def get_smoothed_xysdk_coeffs(fistardir):
+    '''
+    This generates smoothed xy and sdk coefficents for use with iphot later
+    (these go into the output photometry file).
+
+    grtrans ${APPHOT}/$base.${EXT_FISTAR} --col-xy 2,3 --col-fit 6,7,8 \
+                  --col-weight 10 --order 4 \
+                  --iterations 3 --rejection-level 3 \
+                  --comment --output-transformation ${IPHOT}/$base.xysdk
+
+    '''
+
+
+def get_reference_frame_shifts():
+    '''
+    This gets shifts between the astrometric reference frame and all other
+    frames.
+
+    '''
+
+
+def transform_frames_to_astrometric_reference():
+    '''
+    This shifts all frames to the astrometric reference.
+
+    '''
+
+
+##################################
+## PHOTOMETRIC REFERENCE FRAMES ##
+##################################
+
+def select_photometric_reference_frames(photdir,
+                                        minframes=80):
+    '''
+    This selects a group of photometric reference frames that will later be
+    stacked and medianed to form the single photometric reference frame.
+
+    '''
+
+
+
+def stack_photometric_reference_frames(framelist):
+    '''
+    This stacks the photometric reference frames in median mode.
+
+    - first, transform all of them to the astrometric reference frame
+    - then use fiarith in median mode to stack them
+
+    '''
+
+
+def photometry_on_stacked_photref(
+    stackedphotref,
+    apertures='2.95:7.0:6.0,3.35:7.0:6.0,3.95:7.0:6.0'
+    ):
+    '''
+    This runs fiphot in the special iphot mode on the stacked photometric
+    reference frame.
+
+    '''
+
+def generate_photref_registration(stackedphotref):
+    '''
+    This generates a registration file for use with convolution later.
+
+    '''
+
+
+#################################
+## CONVOLUTION AND SUBTRACTION ##
+#################################
+
+def convolve_and_subtract_frames(transformedframelist,
+                                 stackedphotref,
+                                 stackedphotrefregistration,
+                                 kernelspec='b/4;i/4;d=4/4'):
+    '''
+    This convolves the photometric reference to each frame, using the specified
+    kernel, then subtracts the frame from the photometric reference to produce
+    the subtracted frames.
+
+    '''
+
+def photometry_on_subtracted_frames(subtractedframelist,
+                                    photrefrawphot):
+    '''
+    This runs photometry on the subtracted frames and finally produces the ISM
+    magnitudes.
+
+    See run_iphot.py and IMG-3-PHOT_st5.sh for what this is supposed to do.
+
+    '''
 
