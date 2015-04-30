273fbbed5fac116d245722dc575a48f7301b9dde
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2015-04-30 14:27:32 -0400

    fixing bugs with LC collection

--- imagesubphot.py
+++ imagesubphot.py
@@ -2143,8 +2143,10 @@ def parallel_collect_imagesublcs(framedir,
         hatids = [rows[0] for x in rows]
         db.close()
 
+        print(hatid[:10])
+
         # generate the task list
-        tasks = [(hatid[0],
+        tasks = [(hatid,
                   photindexdb,
                   outdir,
                   {'skipcollected':skipcollectedlcs,
