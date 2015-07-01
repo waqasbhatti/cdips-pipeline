d38c38fb21ded8abbe34f79dfa884ffb54f0f3f6
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2015-07-01 12:16:30 -0400

    various fixes

--- aperturephot.py
+++ aperturephot.py
@@ -5493,6 +5493,7 @@ def plot_magrms_comparison(reference_stats_file,
             )
 
         nonzero_ind = np.where(tf3_compcol_ratios > 0.0)
+        print(len(nonzero_ind[0]), type(nonzero_ind[0])
         xcol, ycol = (np.array(ref_tf3_mag[nonzero_ind[0]]),
                       tf3_compcol_ratios[nonzero_ind[0]])
 
