2fd5759e952186a174fe2c82141d10dcda748476
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2015-07-01 16:14:58 -0400

    imagesubphot: trying to fix photref selection

--- imagesubphot.py
+++ imagesubphot.py
@@ -1207,7 +1207,7 @@ def select_photref_frames(fitsdir,
 
     # this is the final operating set of frames that will be sorted for the
     # following tests
-    selectind = haind & moonind & zenithind
+    selectind = haind & moonind & zenithind & backgroundstdevind
 
     selected_frames = infodict['frames'][selectind]
     selected_ngoodobj = infodict['ngoodobjs'][selectind]
@@ -1235,11 +1235,11 @@ def select_photref_frames(fitsdir,
     # first sort selector
     stage1_sort_ind = (np.argsort(selected_medsvalue))[::-1]
 
-    stage1_frames = selected_frames[stage1_sort_ind[:3*minframes]]
-    stage1_median_bgv = selected_medsrcbkg[stage1_sort_ind[:3*minframes]]
-    stage1_stdev_bgv = selected_stdsrcbkg[stage1_sort_ind[:3*minframes]]
-    stage1_svalue = selected_medsvalue[stage1_sort_ind[:3*minframes]]
-    stage1_dvalue = selected_meddvalue[stage1_sort_ind[:3*minframes]]
+    stage1_frames = selected_frames[stage1_sort_ind[:2*minframes]]
+    stage1_median_bgv = selected_medsrcbkg[stage1_sort_ind[:2*minframes]]
+    stage1_stdev_bgv = selected_stdsrcbkg[stage1_sort_ind[:2*minframes]]
+    stage1_svalue = selected_medsvalue[stage1_sort_ind[:2*minframes]]
+    stage1_dvalue = selected_meddvalue[stage1_sort_ind[:2*minframes]]
 
     # next, sort by roundest stars
     stage2_sort_ind = (np.argsort(np.fabs(stage1_dvalue)))
@@ -1248,7 +1248,7 @@ def select_photref_frames(fitsdir,
     stage2_median_bgv = stage1_median_bgv[stage2_sort_ind]
     stage2_stdev_bgv = stage1_stdev_bgv[stage2_sort_ind]
     stage2_svalue = stage1_svalue[stage2_sort_ind]
-    stage2_dvalue = stage1_svalue[stage2_sort_ind]
+    stage2_dvalue = stage1_dvalue[stage2_sort_ind]
 
     final_bgvmed_ind = stage2_median_bgv < maxbackgroundmedian
     final_bgvstd_ind = stage2_stdev_bgv < maxbackgroundstdev
