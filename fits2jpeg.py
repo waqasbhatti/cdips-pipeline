e394ceb757900ae6be7ce80d4844841f7f0f6dc0
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2015-11-15 19:13:56 -0500

    added fits2jpeg and fitshdr executables

---
+++ fits2jpeg.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+'''
+fits2jpeg.py - Waqas Bhatti (waqas@astro.princeton.edu) - Jan 2013
+
+This converts a FITS file or a list of FITS to JPEGs. Be default, tries to be
+smart about the FITS image type. For FITS with imagetypes of 'object' or
+'focus', it makes 3 x 3 stamp JPEGs of the image. For FITS of any other
+imagetype, it makes full frame JPEGS.
+
+'''
+
+import optparse
+import os
+import os.path
+import sys
+
+import imageutils
+
+if __name__ == '__main__':
+
+    # parse the command line
+    usage = 'Usage: %prog [options] [FITS file to convert] [output filename]'
+    options = optparse.OptionParser(usage=usage)
+    options.add_option('-t','--type',
+                       dest='out_type',
+                       action='store',
+                       type='string',
+                       default=None,
+                       help=('type of output to create: is one of [stamp, full]. '
+                             'by default, fits2jpeg figures this out automatically '
+                             'based on the IMAGETYP keyword in the FITS header.'))
+    opts, args = options.parse_args()
+
+    if len(args) < 1:
+        options.error('need a FITS file to work on')
+    elif len(args) < 2:
+        options.error('need an output filename')
+
+    in_file, out_file = args
+
+    if opts.out_type is None:
+        imageutils.fits_to_jpeg(in_file,out_fname=out_file)
+    elif opts.out_type == 'stamp':
+        imageutils.fits_to_stamps_jpeg(in_file,out_fname=out_file)
+    elif opts.out_type == 'full':
+        imageutils.fits_to_full_jpeg(in_file,out_fname=out_file)
+    else:
+        print('unknown output type %s' % opts.out_type)
+        sys.exit(1)
+
---
+++ fitshdr.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+'''
+fitshdr.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - Jan 2013
+
+Simple utility to read and display the image header for a FITS file. Gets rid of
+blank lines in the header by default, set the --full command line option to show
+these as well.
+
+'''
+
+import optparse
+import os
+import os.path
+import sys
+
+import pyfits
+
+
+if __name__ == '__main__':
+
+    # parse the command line
+    usage = 'Usage: %prog [options] [FITS file to read]'
+    options = optparse.OptionParser(usage=usage)
+    options.add_option('-f','--full',
+                       dest='full_header',
+                       action='store_true',
+                       default=False,
+                       help='show the full header, including all blank lines')
+    options.add_option('-x','--ext',
+                       dest='read_ext',
+                       action='store',
+                       type='int',
+                       default=None,
+                       metavar='EXT',
+                       help='show header for extension EXT')
+    options.add_option('-k','--keyword',
+                       dest='keyword',
+                       action='store',
+                       type='string',
+                       default=None,
+                       metavar='KEYWORD',
+                       help='show value of FITS keyword KEYWORD')
+    opts, args = options.parse_args()
+    if len(args) < 1:
+        options.error('need at least one FITS file to work on')
+
+    # go through each file in the argument list
+    for fits_file in args:
+
+        hdu = pyfits.open(fits_file)
+        n_ext = len(hdu)
+
+        if opts.read_ext is not None:
+
+            ext = hdu[opts.read_ext]
+
+            if opts.keyword is not None:
+
+                try:
+                    keyword_val = ext.header[opts.keyword]
+                    print('%s: %s'% (fits_file, keyword_val))
+
+                except KeyError:
+                    print('keyword %s not found' % opts.keyword)
+
+            else:
+
+                header = ext.header.tostring(padding=False,
+                                             sep='\n',
+                                             endcard=False)
+
+                header = header.split('\n')
+                header = [x.strip() for x in header]
+
+                if not opts.full_header:
+                    header = [x for x in header if len(x) > 0]
+
+                print('\nfile: %s, header for extension %i:' % (fits_file,
+                                                                opts.read_ext))
+                for card in header:
+                    print(card)
+
+        else:
+
+            for i,ext in enumerate(hdu):
+
+                data_length = ext.fileinfo()['datSpan']
+
+                if data_length > 0:
+
+                    if opts.keyword is not None:
+
+                        try:
+                            keyword_val = ext.header[opts.keyword]
+                            print('%s: %s'% (fits_file, keyword_val))
+
+                        except KeyError:
+                            print('keyword %s not found' % opts.keyword)
+
+
+                    else:
+
+                        header = ext.header.tostring(padding=False,
+                                                     sep='\n',
+                                                     endcard=False)
+
+                        header = header.split('\n')
+                        header = [x.strip() for x in header]
+
+                        if not opts.full_header:
+                            header = [x for x in header if len(x) > 0]
+
+                        print('\nfile: %s, header for extension %i:' %
+                              (fits_file,i))
+                        for card in header:
+                            print(card)
+
+        hdu.close()
+
