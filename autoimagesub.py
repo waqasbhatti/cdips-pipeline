2f694d16634f060c43215308da09d2cf36d9cb12
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-09-06 16:41:22 -0400

    added skeleton of new autoimagesub.py

---
+++ autoimagesub.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''autoimagesub.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - 09/2016
+
+This contains functions to run image subtraction photometry continuously.
+
+TODO:
+
+- functions for running imagesub steps on lists of reduced FITS
+- functions that search for reference images
+- functions that search for reference image photometry catalogs
+- function that knows how to time-out a subprocess call to ficonv
+
+'''
