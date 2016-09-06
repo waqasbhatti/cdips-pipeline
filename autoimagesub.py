21403dc19b74e49725d77833deff66c8df6110b9
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-09-06 17:03:21 -0400

    more skeletons

--- autoimagesub.py
+++ autoimagesub.py
@@ -12,3 +12,60 @@ TODO:
 - function that knows how to time-out a subprocess call to ficonv
 
 '''
+
+#############
+## IMPORTS ##
+#############
+
+import os
+import os.path
+import glob
+import multiprocessing as mp
+import subprocess
+from subprocess import check_output
+import shlex
+from datetime import datetime
+import re
+import json
+import shutil
+import random
+import cPickle as pickle
+
+
+import aperturephot as ap
+import imagesubphot as ism
+
+
+############
+## CONFIG ##
+############
+
+DEBUG = False
+
+# used to get the station ID, frame number, and CCD number from a FITS filename
+FRAMEREGEX = re.compile(r'(\d{1})\-(\d{6}\w{0,1})_(\d{1})')
+
+# this defines the field string and CCDs
+FIELD_REGEX = re.compile('^G(\d{2})(\d{2})([\+\-]\d{2})(\d{2})_(\w{3})$')
+FIELD_CCDS = [5,6,7,8]
+FIELD_REFBASEDIR = '/P/HP0/BASE/reference-frames'
+
+
+######################
+## REFERENCE FRAMES ##
+######################
+
+
+def find_reference_frame(field,
+                         refbasedir=FIELD_REFBASEDIR,
+                         refver='latest'):
+    '''
+    This finds the reference frame for the field.
+
+    reference frames are found in:
+
+    {REFBASEDIR}/{field}
+
+
+
+    '''
