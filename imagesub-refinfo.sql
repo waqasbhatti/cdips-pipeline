5efc56d122366da74ea4efcc5fa81a2209d52238
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-09-07 12:55:44 -0400

    update refinfo DB schema

--- imagesub-refinfo.sql
+++ imagesub-refinfo.sql
@@ -1,3 +1,46 @@
 -- database to use for automatic ISM photometry
-CREATE TABLE astromrefs (field text, projectid integer, ccd integer, isactive integer, unixtime double precision, framepath text, jpegpath text, sval real, dval real, bgv real, ndet integer, comment text);
-CREATE TABLE photrefs (field text, projectid integer, ccd integer, isactive integer, unixtime double precision, framepath text, jpegpath text, convolvetarget text, convolveregpath text, cmrawphotpath text, target_zenithdist real, target_moondist real, target_moonelev real, target_moonphase real, target_hourangle real, target_ndet real, target_medmagerr real, target_magerrmad real, target_medsrcbgv, target_stdsrcbgv real, target_medsval real, target_meddval real);
+
+-- for astrometric reference selection per field, ccd, and projectid
+CREATE TABLE astromrefs (
+       field text,
+       projectid integer,
+       ccd integer,
+       isactive integer,
+       unixtime double precision,
+       framepath text,
+       jpegpath text,
+       sval real,
+       dval real,
+       bgv real,
+       ndet integer,
+       comment text,
+       primary key(field, projectid, ccd, isactive)
+);
+
+
+-- for combined photometric reference selection per field, ccd, and projectid
+CREATE TABLE photrefs (
+       field text,
+       projectid integer,
+       ccd integer,
+       isactive integer,
+       unixtime double precision,
+       framepath text,
+       jpegpath text,
+       convolvetarget text,
+       convolveregpath text,
+       cmrawphotpath text,
+       target_zenithdist real,
+       target_moondist real,
+       target_moonelev real,
+       target_moonphase real,
+       target_hourangle real,
+       target_ndet real,
+       target_medmagerr real,
+       target_magerrmad real,
+       target_medsrcbgv real,
+       target_stdsrcbgv real,
+       target_medsval real,
+       target_meddval real,
+       primary key(field, projectid, ccd, isactive)
+);
