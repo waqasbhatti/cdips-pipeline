d0d1f889f18783d127b6193d21b7ff5453a8aa3b
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-09-07 12:56:01 -0400

    implemented generate_astromref

--- autoimagesub.py
+++ autoimagesub.py
@@ -30,6 +30,8 @@ import json
 import shutil
 import random
 import cPickle as pickle
+import sqlite3
+import time
 
 
 import aperturephot as ap
@@ -60,6 +62,7 @@ REFINFO = os.path.join(FIELD_REFBASEDIR,'refinfo.sqlite')
 ######################
 
 def generate_astromref(fitsfiles,
+                       makeactive=True,
                        field=None,
                        ccd=None,
                        projectid=None,
@@ -76,7 +79,116 @@ def generate_astromref(fitsfiles,
     if field, ccd, or projectid are None, these values are taken from the FITS
     file headers.
 
+    updates the refinfo database.
+
     '''
+
+    goodfits = [x for x in fitsfiles if os.path.exists(x)]
+
+    if not goodfits:
+        print('ERR! %sZ: no good FITS files found in input list' %
+              (datetime.utcnow().isoformat(),))
+        return
+
+    # find the astromref
+    astromref = ism.select_astromref_frame(
+        fitsfiles,
+        '1-*.fits',
+    )
+
+    # if an astromref was successfully found, then add its info to the DB
+    if astromref:
+
+        if field and ccd and projectid:
+
+            frameinfo = {'field':field,
+                         'ccd':ccd,
+                         'projectid':projectid}
+
+        else:
+
+            # get the frame info
+            frameelems = get_header_keyword_list(astromref,
+                                                 ['object',
+                                                  'projid'])
+
+            felems = FRAMEREGEX.findall(os.path.basename(astromref))
+
+            if felems and felems[0]:
+
+                ccd = felems[0][2]
+                frameinfo = {'field':frameelems['object'],
+                             'ccd':ccd,
+                             'projectid':frameelems['projid']}
+
+            else:
+
+                print('ERR! %sZ: could not figure out CCD for astromref: %s' %
+                      (datetime.utcnow().isoformat(), astromref['astromref']))
+                return
+
+            # now, put together the information and write to the refinfo sqlite
+
+            query = ("insert into astromrefs "
+                     "(field, projectid, ccd, isactive, unixtime, "
+                     "framepath, jpegpath, sval, dval, bgv, ndet, "
+                     "comment) values "
+                     "(?, ?, ?, ?, ?, "
+                     "?, ?, ?, ?, ?, "
+                     "?)")
+            params = (frameinfo['field'],
+                      frameinfo['projectid'],
+                      frameinfo['ccd'],
+                      1 if makeactive else 0,
+                      time.time(),
+
+                      astromref['astromref'],
+                      astromref['framejpg'],
+                      astromref['sval'],
+                      astromref['dval'],
+                      astromref['bgv'],
+                      astromref['ndet'],
+
+                      astromref['comment'])
+
+            db = sqlite3.connect(refinfo)
+            cur = db.cursor()
+
+            try:
+
+                astromref.update(frameinfo)
+                cur.execute(query, params)
+                db.commit()
+
+                print('%sZ: using astromref %s for '
+                      'field %s, ccd %s, project id %s, database updated.' %
+                      (datetime.utcnow().isoformat(),
+                       astromref['astromref'],
+                       astromref['field'],
+                       astromref['ccd'],
+                       astromref['projectid']))
+
+                returnval = astromref
+
+            except Exception as e:
+
+                print('ERR! %sZ: could not update refinfo DB! error was: %s' %
+                      (datetime.utcnow().isoformat(), e))
+                returnval = None
+                db.rollback()
+
+            db.close()
+
+    # otherwise, do nothing
+    else:
+
+        print('ERR! %sZ: could not find an astromref frame' %
+              (datetime.utcnow().isoformat(),))
+        returnval = None
+
+
+    return returnval
+
 
 
 
