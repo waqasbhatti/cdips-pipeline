7ee707681a19f9a46558b728377f233015797308
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-09-08 12:43:56 -0400

    added function for getting frame info

--- autoimagesub.py
+++ autoimagesub.py
@@ -491,6 +491,139 @@ def framelist_make_xtrnsfits(fitsfiles,
 ## PHOTOMETRIC REFERENCE FRAMES ##
 ##################################
 
+def get_frame_info_for_photref_candidates(frame):
+    '''
+    This gets the needed info from a frame for selecting photref candidates.
+
+    '''
+
+    try:
+
+        # find the frame's fistar and fiphot files
+        fitsdir = os.path.dirname(frame)
+        fitsbase = os.path.splitext(os.path.basename(frame))[0]
+
+        # if the xtrns files are passed in, make sure we look at the
+        # right fistar and fiphot files
+        if '-xtrns' in fitsbase:
+            fitsbase = fitsbase.rstrip('-xtrns')
+
+        photpath = os.path.join(
+            fitsdir,
+            fitsbase + '.fiphot'
+            )
+        srclistpath = os.path.join(
+            fitsdir,
+            fitsbase + '.fistar'
+            )
+
+        if not (os.path.exists(frame) and
+                os.path.exists(photpath) and
+                os.path.exists(srclistpath)):
+
+            print('ERR! %sZ: frame %s has missing fiphot/fistar, skipping...' %
+                  (datetime.utcnow().isoformat(), frame))
+            return frame, None
+
+
+
+        # 1. get the data from FITS header
+        headerdata = imageutils.get_header_keyword_list(
+            frame,
+            ['Z','MOONDIST','MOONELEV','MOONPH','HA']
+        )
+
+        # 2. get the data from the fiphot file
+
+        # decide if the phot file is binary or not. read the first 600
+        # bytes and look for the '--binary-output' text
+        with open(photpath,'rb') as photf:
+            header = photf.read(1000)
+
+        if '--binary-output' in header and HAVEBINPHOT:
+
+            photdata_f = read_fiphot(photpath)
+            photdata = {
+                'mag':np.array(photdata_f['per aperture'][2]['mag']),
+                'err':np.array(photdata_f['per aperture'][2]['mag err']),
+                'flag':np.array(
+                    photdata_f['per aperture'][2]['status flag']
+                    )
+                }
+            del photdata_f
+
+        elif '--binary-output' in header and not HAVEBINPHOT:
+
+            print('WRN! %sZ: %s is a binary phot file, '
+                  'but no binary phot reader is present, skipping...' %
+                  (datetime.utcnow().isoformat(), photpath))
+            continue
+
+        else:
+
+            # read in the phot file
+            photdata = np.genfromtxt(
+                photpath,
+                usecols=(12,13,14),
+                dtype='f8,f8,S5',
+                names=['mag','err','flag']
+                )
+
+        # 3. get the data from the fistar file
+        srcdata = np.genfromtxt(srclistpath,
+                                usecols=(3,5,6),
+                                dtype='f8,f8,f8',
+                                names=['background',
+                                       'svalue',
+                                       'dvalue'])
+
+        # process fiphot data
+        if '--binary-output' in header:
+            goodind = np.where(photdata['flag'] == 0)
+        else:
+            goodind = np.where(photdata['flag'] == 'G')
+
+        median_mag = np.median(photdata['mag'][goodind])
+
+        # these are the quantities we're interested in
+        ngood = len(goodind[0])
+        median_magerr = np.nanmedian(photdata['err'][goodind])
+        medabsdev_mag = np.nanmedian(
+            np.abs(photdata['mag'][goodind] - median_mag)
+            )
+
+
+        # now consolidate all the data
+        frameinfo = {
+            'zenithdist':(headerdata['Z']
+                          if 'Z' in headerdata else np.nan),
+            'moondist':(headerdata['MOONDIST']
+                        if 'MOONDIST' in headerdata else np.nan),
+            'moonelev':(headerdata['MOONELEV']
+                        if 'MOONELEV' in headerdata else np.nan),
+            'moonphase':(headerdata['MOONPH']
+                         if 'MOONPH' in headerdata else np.nan),
+            'hourangle':(headerdata['HA']
+                         if 'HA' in headerdata else np.nan),
+            'ngoodobjects':ngood,
+            'medmagerr':median_magerr,
+            'magerrmad':medabsdev_mag,
+            'medsrcbgv':np.nanmedian(srcdata['background']),
+            'stdsrcbgv':np.nanstd(srcdata['background']),
+            'medsval':np.nanmedian(srcdata['svalue']),
+            'meddval':np.nanmedian(srcdata['dvalue']),
+        }
+
+        return frame, frameinfo
+
+    except Exception as e:
+
+        print('ERR! %sZ: could not get info from frame %s, error was: %s' %
+              (datetime.utcnow().isoformat(), frame, e))
+        return frame, None
+
+
+
 def generate_photref_candidates_from_xtrns(fitsfiles,
                                            makeactive=True,
                                            minframes=50,
@@ -509,6 +642,8 @@ def generate_photref_candidates_from_xtrns(fitsfiles,
     for this operation to make sense.
 
     '''
+
+
 
 
 def generate_combined_photref(photreftarget,
