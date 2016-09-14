4c09cffde99e42872b9d4fb4f077cd7d7c00ca8d
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-09-14 16:57:29 -0400

    added postgres stuff

--- autoimagesub.py
+++ autoimagesub.py
@@ -84,7 +84,7 @@ with open(PGPASSFILE) as infd:
     pgpass_contents = infd.readlines()
     pgpass_contents = [x.split(':') for x in pgpass_contents]
     PGPASSWORD = [x[-1] for x in pgpass_contents
-                  if (x[2] == PGDATABSE and x[3] == PGUSER)]
+                  if (x[2] == PGDATABASE and x[3] == PGUSER)]
 
 
 ###############
