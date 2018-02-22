06eb6d8df09a4721cf516ff96fab6f8721b9761f
lgbouma <lgbouma@mit.edu>
2018-02-22 09:52:39 -0500

    fix shared global declarations

--- shared_globals.py
+++ shared_globals.py
@@ -11,13 +11,13 @@ FIELD_REGEX and FIELD_CCDS defines the field string and CCDs
 '''
 import re
 
-LOCAL_IMGBASE = '/nfs/phtess1/ar1/HATPI/HP0/RAW/',
-LOCAL_GLOBPATTERN = '?-????????',
-FIELDCAT_DIR = '/nfs/phtess1/ar1/HATPI/HP0/BASE/CAT',
-FIELDCAT_FOV = 14.0,
-FIELDCAT_BRIGHT = 0.0,
-FIELDCAT_FAINT = 15.5,
-FIELDCAT_BANDS = ['g,r,i','r,i,z'],
-FIELD_REGEX = re.compile('^G(\d{2})(\d{2})([\+\-]\d{2})(\d{2})_(\w{3})$'),
+LOCAL_IMGBASE = '/nfs/phtess1/ar1/HATPI/HP0/RAW/'
+LOCAL_GLOBPATTERN = '?-????????'
+FIELDCAT_DIR = '/nfs/phtess1/ar1/HATPI/HP0/BASE/CAT'
+FIELDCAT_FOV = 14.0
+FIELDCAT_BRIGHT = 0.0
+FIELDCAT_FAINT = 15.5
+FIELDCAT_BANDS = ['g,r,i','r,i,z']
+FIELD_REGEX = re.compile('^G(\d{2})(\d{2})([\+\-]\d{2})(\d{2})_(\w{3})$')
 FIELD_CCDS = [5,6,7,8]
 
