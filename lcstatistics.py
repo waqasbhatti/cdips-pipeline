044b88f959b6429beabee6cbd7a05110f8178104
lgbouma <lgbouma@mit.edu>
2018-12-23 11:43:05 -0500

    lcstatistics: change whisker plots to percentiles
    more readable and understandable to just look at lines, rather than
    weird boxes where different features mean different things.

--- lcstatistics.py
+++ lcstatistics.py
@@ -3,8 +3,8 @@ from __future__ import division, print_function
 '''
 functions for assessing statistics of lightcurves you've made.
 
-whisker_MAD_stats_and_plots:
-    make csv files and (optionally) whisker plots of MAD vs magnitude.
+percentiles_MAD_stats_and_plots:
+    make csv files and (optionally) percentiles plots of MAD vs magnitude.
 
 TODO:
 move items from aperturephot.py here, and update all respective calls.
@@ -24,6 +24,8 @@ import aperturephot as ap
 from glob import glob
 from astropy import units as units, constants as constants
 from datetime import datetime
+
+from numpy import array as nparr
 
 ################################
 # LIGHTCURVE READING FUNCTIONS #
@@ -97,76 +99,15 @@ def read_tfa_lc(tfafile,
     return lcdata
 
 
-####################
-# HELPER FUNCTIONS #
-####################
-
-def _customized_box_plot(percentiles, axes, redraw = True, *args, **kwargs):
-    '''
-    Generates a customized boxplot based on the given percentile values. Stolen
-    from
-
-    `https://stackoverflow.com/questions/27214537/is-it-possible-to-draw-a-matplotlib-boxplot-given-the-percentile-values-instead`
-    '''
-
-    # Create len(percentiles) no of box plots
-    n_box = len(percentiles)
-    box_plot = axes.boxplot([[-9, -4, 2, 4, 9],]*n_box, *args, **kwargs)
-
-    min_y, max_y = float('inf'), -float('inf')
-
-    for box_no, (q1_start,
-                 q2_start,
-                 q3_start,
-                 q4_start,
-                 q4_end
-                 ) in enumerate(percentiles):
-
-        # Lower cap
-        box_plot['caps'][2*box_no].set_ydata([q1_start, q1_start])
-        # xdata is determined by the width of the box plot
-
-        # Lower whiskers
-        box_plot['whiskers'][2*box_no].set_ydata([q1_start, q2_start])
-
-        # Higher cap
-        box_plot['caps'][2*box_no + 1].set_ydata([q4_end, q4_end])
-
-        # Higher whiskers
-        box_plot['whiskers'][2*box_no + 1].set_ydata([q4_start, q4_end])
-
-        # Box
-        box_plot['boxes'][box_no].set_ydata([q2_start,
-                                             q2_start,
-                                             q4_start,
-                                             q4_start,
-                                             q2_start])
-
-        # Median
-        box_plot['medians'][box_no].set_ydata([q3_start, q3_start])
-
-        min_y = min(q1_start, min_y)
-        max_y = max(q4_end, max_y)
-
-        # The y axis is rescaled to fit the new box plot completely with 10% 
-        # of the maximum value at both ends
-        axes.set_ylim([min_y*1.2, max_y*1.1])
-
-    # If redraw is set to true, the canvas is updated.
-    if redraw:
-        axes.figure.canvas.draw()
-
-    return box_plot
-
-
 ######################
 # PLOTTING FUNCTIONS #
 ######################
 
-def whisker_MAD_stats_and_plots(statdir, outprefix, binned=False,
-                                make_whisker_plot=True, whisker_xlim=[4,17],
-                                whisker_ylim=[1e-5,1e-1],
-                                percentiles=[2,25,50,75,98]):
+def percentiles_MAD_stats_and_plots(statdir, outprefix, binned=False,
+                                    make_percentiles_plot=True,
+                                    percentiles_xlim=[4,17],
+                                    percentiles_ylim=[1e-5,1e-1],
+                                    percentiles=[2,25,50,75,98]):
     '''
     make csv files and (optionally) plots of MAD vs magnitude statistics,
     evaluated at percentiles.
@@ -187,7 +128,7 @@ def whisker_MAD_stats_and_plots(statdir, outprefix, binned=False,
     else:
         stats = ap.read_stats_file(tfastatfile)
 
-    # MAD vs magnitude statistics, at 1-mag differences, summarized as whiskers
+    # MAD vs magnitude statistics, at 1-mag differences, summarized as percentiles
     # at 2%, 25%, 50%, 75%, and 98% percentiles.
     for apstr in ['tf1','tf2','tf3',
                   'rm1','rm2','rm3',
@@ -218,40 +159,37 @@ def whisker_MAD_stats_and_plots(statdir, outprefix, binned=False,
 
             pctile_df = pd.DataFrame(percentile_dict)
 
-            if make_whisker_plot:
+            if make_percentiles_plot:
+                import itertools
 
                 plt.close('all')
                 fig, ax = plt.subplots(figsize=(4,3))
 
-                # whisker plot isn't aware of absolute x values, it just sees them
-                # as different columns. thus we need to pre-select the magnitude
-                # columns according to whisker_xlims that are passed.
-                if whisker_xlim:
-                    sel_cols = (
-                        (pctile_df.columns < max(whisker_xlim)) &
-                        (pctile_df.columns > min(whisker_xlim))
-                    )
-                    # surprising that i couldn't figure out a smarter way ._.
-                    plotdf = pctile_df.transpose()[sel_cols].transpose()
+                # this was originally a bonafide percentiles plot. however getting
+                # it to look right was surprisingly annoying.
+                # this plot is therefore just lines for each percentile in
+                # [2,25,50,75,98], binned for every magnitude interval.
 
-                boxdata = np.array(plotdf).T
+                markers = itertools.cycle(('o', 'v', '>', 'D', 's', 'P'))
 
-                b = _customized_box_plot(
-                    boxdata, ax, redraw=True, notch=0, vert=1, whis=1.5
-                )
+                for ix, row in pctile_df.iterrows():
+                    pctile = row.name
+                    label = '{}%'.format(str(pctile))
+
+                    midbins = nparr(row.index)
+                    vals = nparr(row)
+
+                    ax.plot(midbins, vals, label=label, marker=next(markers))
+
+                ax.legend(loc='best', fontsize='xx-small')
 
                 ax.set_yscale('log')
                 ax.set_xlabel('{:s} median instrument magnitude'.format(apstr.upper()))
                 ax.set_ylabel('{:s} median abs. dev.'.format(apstr.upper()))
-                xticklabels = []
-                for ix, m in enumerate(np.mean(mag_bins, axis=1)):
-                    if ix%2==0:
-                        xticklabels.append(m)
-                    else:
-                        xticklabels.append('')
-                ax.set_xticklabels(xticklabels)
-                if whisker_ylim:
-                    ax.set_ylim(whisker_ylim)
+                if percentiles_xlim:
+                    ax.set_xlim(percentiles_xlim)
+                if percentiles_ylim:
+                    ax.set_ylim(percentiles_ylim)
 
                 titlestr = '{:s} - {:d} - {:s}'.format(
                     outprefix,
@@ -267,7 +205,7 @@ def whisker_MAD_stats_and_plots(statdir, outprefix, binned=False,
                                linestyle=':')
 
                 savname = ( os.path.join(
-                    statdir,'whisker_MAD_vs_med_mag_{:s}.png'.format(apstr.upper())
+                    statdir,'percentiles_MAD_vs_med_mag_{:s}.png'.format(apstr.upper())
                 ))
                 fig.tight_layout()
                 fig.savefig(savname, dpi=250)
@@ -275,13 +213,13 @@ def whisker_MAD_stats_and_plots(statdir, outprefix, binned=False,
                       (datetime.utcnow().isoformat(), titlestr, savname))
 
             csvname = ( os.path.join(
-                statdir,'whisker_MAD_vs_med_mag_{:s}.csv'.format(apstr.upper())
+                statdir,'percentiles_MAD_vs_med_mag_{:s}.csv'.format(apstr.upper())
             ))
             pctile_df.to_csv(csvname, index=False)
             print('%sZ: wrote %s' %
                   (datetime.utcnow().isoformat(), csvname))
         except Exception as e:
-            print('%sZ: failed to make whisker for %s, err was %s' %
+            print('%sZ: failed to make percentiles for %s, err was %s' %
                   (datetime.utcnow().isoformat(), apstr, e))
 
 
