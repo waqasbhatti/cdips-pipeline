0d68e9545bec7129518989e5fb5924e38ebe2094
lgbouma <lgbouma@mit.edu>
2019-05-22 09:38:34 -0400

    tess: add some badtimes

--- tessutils.py
+++ tessutils.py
@@ -163,7 +163,10 @@ badtimewindows = [
     (1406.29246, 1409.388295), # sector 3, orbit 14 end
     (1418.53690, 1421.211685), # sector 4 instrument anomaly
     (1465.212615, 1468.269985), # sector 6 PRF time
-    (1477.01998, 1478.11304), # sector 6 downlink
+    (1477.01998, 1478.11304), # sector 6 downlink, btwn orbits 19->20
+    (1503.03803, 1504.69775), # sector 7 downlink, btwn orbits 21->22
+    (1529.06510, 1530.25816), # sector 8 downlink, btwn orbits 23->24
+    (1531.74288, 1535.00264)  # sector 8 instrument anomaly
 ]
 
 def mask_dquality_flag_frame(task):
