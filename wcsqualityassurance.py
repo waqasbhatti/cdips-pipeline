d36715702f1c1a6ea9c917fbc358fa4fb5cdc63f
lgbouma <lgbouma@mit.edu>
2019-05-27 15:47:05 -0400

    wcs quality assurance: still WIP. see notes below.
    For TESS images,
    default astrometry.net settings produce astrometric "solutions" that are
    precise to often much worse than 1 pixel.
    
    For forced aperture photometry on projected positions, this is not
    precise enough.
    
    I tried messing with about every switch available for `solve-field`.
    
    The following can be useful:
    
    * `--pixel-error`: tuning below 1, down to about 0.2, can improve the
            astrometric residual.
    
    *  `--uniformize`: tuning between 10 and 15 can improve the residual.
    
    * `--tweak-order`: 6 provided slight improvements over 4.
    
    * `--downsample`: for TESS, setting this to 2 always produced best fits.
            If it is not set, then `solve-field` does not converge.
    
    The following had basically no effect, except possibly negative:
    `--quad-size-min`, `--quad-size-max` `--code-tolerance`, and all of the
    `--odds-to-*` options.  Tuning the `--depth` also had no helpful effect
    -- the default astrometry.net behavior of just advancing to fainter
    stars if bright stars do not produce good quads seems to work fine.
    
    Setting `--crpix-center` improved the residual near the center CRPIX,
    but introduced a coherent ~0.5 pixel rotational distortion at the field
    corners.
    
    At best, for a typical TESS image, best solutions came from calls like
    
    ```
      solve-field --ra 85.1054 --dec -24.6352 --radius 20 --scale-low 15 \
      --scale-high 25 --scale-units arcsecperpix --tweak-order 6 --wcs \
      /home/lbouma/tess2018353175939-s0006-2-1-0126_cal_img_bkgdsub.wcs \
      --downsample 2 --overwrite --fits-image --no-verify
      --pixel-error 0.15 \
      --uniformize 10 --quad-size-min 0.005 --quad-size-max 0.1 \
      /home/lbouma/tess2018353175939-s0006-2-1-0126_cal_img_bkgdsub.fits
    ```
    
    which gave residuals of median ~0.10-0.11 pixels, mean ~0.18 pixels, std
    ~0.77 pixels.
    
    Compared to the SPOC WCS for the same, which gives median ~0.07 pixels,
    mean ~0.16 pixels, std ~0.80 pixels.
    
    Possible TODOs include (a) use fistar to feed x,y lists instead of
    relying on astrometry.net source extractor; (b) split FFIs into 2x2, or
    3x3 subimages, to lower the dependence on the nonlinear SIP terms in the
    field corners.  (c) An alternative would be to adopt the SPOC WCSs, if
    they could be shown to work well on the photref and astromref frames.

--- wcsqualityassurance.py
+++ wcsqualityassurance.py
@@ -5,42 +5,142 @@ size, this is a big no-no).
 """
 import pandas as pd, numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import os
+from numpy import array as nparr
 
-from aperturephot import make_frameprojected_catalog
+from astropy.io import fits
+from astropy import wcs
+
+from aperturephot import (
+    make_frameprojected_catalog,
+    extract_frame_sources,
+    astrometrydotnet_solve_frame
+)
 
 def main():
 
-    #FIXME FIXME: automate the process of generating the .projcat and the
-    #.fistar files. b/c we'll need to tweak the WCS solution, most likely!!
+    ##########################################
+    # change these
+    ##########
 
-    # and/or 
+    # # cam1 ccd4
+    # reformedfovcatalog = '/home/lbouma/GAIADR2-RA86.6153477457052-DEC11.8674772184025-SIZE24.reformed_catalog'
+    # fitsfile = '/home/lbouma/tess2018350135939-s0006-1-4-0126_cal_img_bkgdsub.fits' #cam1ccd4
+    # ra, dec = 86.6153, 11.8674
 
-    if overwrite or not os.path.exists(outprojcat):
-        #FIXME: you can pass the wcs here, to make the projcat files from the
-        # TESS headers!!
-        projcatfile = make_frameprojected_catalog(fits,
+    # # cam3 ccd3
+    # reformedfovcatalog = '/home/lbouma/GAIADR2-RA81.7485401006594-DEC-48.4508505192356-SIZE24.reformed_catalog'
+    # fitsfile = '/home/lbouma/proj1510-s0006-cam3-ccd3-combinedphotref-onenight.fits'
+    # ra, dec = 81.7485, -48.4509
+
+    # cam 2 ccd 1
+    reformedfovcatalog = '/home/lbouma/GAIADR2-RA85.1053734708905-DEC-24.6352467680916-SIZE24.reformed_catalog'
+    fitsfile = '/home/lbouma/tess2018353175939-s0006-2-1-0126_cal_img_bkgdsub.fits'
+    ra, dec = 85.1054, -24.6352
+
+    use_spoc_wcs = 0
+
+    ##########################################
+
+    ##########################################
+    # wont change the following
+    ccdextent = {'x':[0.,2048.],'y':[0.,2048.]}
+    pixborders = 0.0
+
+    gain = 5.35
+    fluxthreshold = 500
+    zeropoint = 11.82
+    exptime = 1800
+
+    outprojcat = fitsfile.replace('.fits','.projcat')
+    fistarpath = fitsfile.replace('.fits','.fistar')
+    matchedpath = fitsfile.replace('.fits','.matched')
+    wcsfile = fitsfile.replace('.fits','.wcs')
+
+    for f in [outprojcat, fistarpath, matchedpath, wcsfile]:
+        if os.path.exists(f):
+            os.remove(f)
+
+    if not os.path.exists(wcsfile) and not use_spoc_wcs:
+        astrometrydotnet_solve_frame(fitsfile,
+                                     wcsfile,
+                                     ra,
+                                     dec,
+                                     radius=20,
+                                     scalelow=20,
+                                     scalehigh=22,
+                                     tweakorder=6,
+                                     useimagenotfistar=True,
+                                     pixelerror=0.15,
+                                     downsample=2,
+                                     uniformize=10)
+    else:
+        write_wcs_from_spoc(fitsfile)
+
+
+    # given WCS and celestial coord catalog, make the frame projected catalog...
+    if not os.path.exists(outprojcat):
+        projcatfile = make_frameprojected_catalog(fitsfile,
                                                   reformedfovcatalog,
                                                   ccdextent=ccdextent,
                                                   out=outprojcat,
-                                                  removetemp=removesourcetemp,
+                                                  wcs=wcsfile,
+                                                  removetemp=True,
                                                   pixborders=pixborders)
-
+    # do source extraction...
+    if not os.path.exists(fistarpath):
+        extract_frame_sources(fitsfile,
+                              fistarpath,
+                              fistarexec='fistar',
+                              ccdextent='0:2048,0:2048',
+                              ccdgain=gain,
+                              fluxthreshold=fluxthreshold,
+                              zeropoint=zeropoint,
+                              exptime=exptime)
     match_and_assess(
-        incatpath = '/home/lbouma/foo.projcat',
+        fitsfile,
+        use_spoc_wcs,
+        incatpath = projcatfile,
         N_bright = 1000,
         N_faint = 9000,
-        fistarpath = '/home/lbouma/foo.fistar',
-        matchedpath = '/home/lbouma/foo.matched'
+        fistarpath = fistarpath,
+        matchedpath = matchedpath
     )
 
 
+def write_wcs_from_spoc(filename):
+
+    # Load the FITS hdulist using astropy.io.fits
+    hdulist = fits.open(filename)
+
+    header = hdulist[0].header
+
+    # # Parse the WCS keywords in the primary HDU
+    # w = wcs.WCS(hdulist[0].header)
+
+    # # Now, write out the WCS object as a FITS header
+    # header = w.to_header()
+
+    # Use header to create a new PrimaryHDU and write it to a file.
+    hdu = fits.PrimaryHDU(header=header)
+
+    # Save to FITS file
+    wcspath = filename.replace('.fits','.wcs')
+    hdu.writeto(wcspath)
+    print('made {} from SPOC header'.format(wcspath))
+
+    hdulist.close()
+
+
 def match_and_assess(
+    fitsfile,
+    use_spoc_wcs,
     incatpath = '/home/lbouma/foo.projcat',
     N_bright = 1000, # say top 1000 are too bright
     N_faint = 9000, # go down to get total of 8000 stars
     fistarpath = '/home/lbouma/foo.fistar',
-    matchedpath = '/home/lbouma/foo.matched'
+    matchedpath = '/home/lbouma/foo.matched',
 ):
     """
     You have a WCS for some image.  You use it to project a background catalog
@@ -90,6 +190,7 @@ def match_and_assess(
     )
 
     rc = os.system(matchcmd)
+    print(matchcmd)
 
     if rc != 0:
       raise RuntimeError('grmatch failed somehow')
@@ -101,6 +202,21 @@ def match_and_assess(
                'Ident,x_meas,y_meas,Bg,Amp,S,D,K,Flux,S/N').split(',')
     )
 
+    if not use_spoc_wcs:
+        # NOTE: means I somehow messed up the coordinates somewhere?!
+        df['x_proj'] -= 0.5
+        df['y_proj'] -= 0.5
+
+    else:
+        # SPOC rows/cols were trimmed, which shifts WCS
+        hdulist = fits.open(fitsfile)
+        hdr = hdulist[0].header
+
+        df['x_proj'] -= (hdr['SCCSA']-1)
+        df['y_proj'] -= (hdr['SCIROWS']-1)
+        df['x_proj'] -= 0.5
+        df['y_proj'] -= 0.5
+
     df['sep'] = sep(
         df['x_meas'],
         df['y_meas'],
@@ -108,15 +224,19 @@ def match_and_assess(
         df['y_proj']
     )
 
-    outpath = os.path.join(os.path.dirname(fistarpath), 'foo_sep_hist.png')
+    pre = os.path.splitext(os.path.basename(fistarpath))[0]
+    if use_spoc_wcs:
+        pre = pre+'_spocwcs'
+    outpath = os.path.join(os.path.dirname(fistarpath),
+                           '{}_sep_hist.png'.format(pre))
     plot_sep_hist(df, outpath)
 
     outpath = os.path.join(os.path.dirname(fistarpath),
-                           'foo_scatter_x_y_sep.png')
+                           '{}_scatter_x_y_sep.png'.format(pre))
     plot_scatter_x_y_sep(df, outpath)
 
     outpath = os.path.join(os.path.dirname(fistarpath),
-                           'foo_quiver_meas_proj_sep.png')
+                           '{}_quiver_meas_proj_sep.png'.format(pre))
     plot_quiver_meas_proj_sep(df, outpath)
 
 
@@ -126,11 +246,15 @@ def sep(x0,y0,x1,y1):
 def plot_sep_hist(df, outpath):
 
     f, ax = plt.subplots(figsize=(4,3))
-    ax.hist(df['sep'], bins=np.logspace(-2, 1, 10))
+
+    weights = np.ones_like(nparr(df['sep']))/float(len(df))
+
+    ax.hist(df['sep'], bins=np.logspace(-2, 1, 19), weights=weights)
+
     ax.text(
         0.02, 0.98,
-        '$\mu$={:.3f}px\n$\sigma$={:.3f}px'.
-        format(df['sep'].mean(),df['sep'].std()),
+        'mean={:.3f}px\nstd={:.3f}px\nmed={:.3f}px'.
+        format(df['sep'].mean(),df['sep'].std(),df['sep'].median()),
         va='top', ha='left',
         transform=ax.transAxes
     )
@@ -143,14 +267,16 @@ def plot_scatter_x_y_sep(df, outpath):
 
     f, ax = plt.subplots(figsize=(4,4))
 
+    norm = mpl.colors.Normalize(vmin=-2.,vmax=1.)
+
     cax = ax.scatter(df['x_meas'], df['y_meas'], c=np.log10(df['sep']),
                      cmap='viridis', s=1, rasterized=True, linewidths=0,
-                     zorder=1)
+                     zorder=1, norm=norm)
 
     ax.set_xlabel('x [px]')
     ax.set_ylabel('y [px]')
 
-    cbar = f.colorbar(cax)
+    cbar = f.colorbar(cax, extend='both')
     cbar.set_label('log10(measured vs proj separation [px])')
 
     f.savefig(outpath, bbox_inches='tight', dpi=300)
@@ -160,19 +286,22 @@ def plot_quiver_meas_proj_sep(df, outpath):
 
     f, ax = plt.subplots(figsize=(4,4))
 
+    norm = mpl.colors.Normalize(vmin=-2.,vmax=1.)
+
     cax = ax.quiver(df['x_meas'], df['y_meas'],
                     df['x_proj']-df['x_meas'],
                     df['y_proj']-df['y_meas'],
                     np.log10(df['sep']),
-                    cmap='viridis')
+                    cmap='viridis',
+                    norm=norm)
 
     ax.set_xlabel('x [px]')
     ax.set_ylabel('y [px]')
 
-    cbar = f.colorbar(cax)
+    cbar = f.colorbar(cax, extend='both')
     cbar.set_label('log10(separation [px])')
 
-    f.savefig(outpath, bbox_inches='tight', dpi=300)
+    f.savefig(outpath, bbox_inches='tight', dpi=400)
 
 
 if __name__ == "__main__":
