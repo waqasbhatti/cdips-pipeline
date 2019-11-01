706db740279bc10541efde1a189110266a53950a
lgbouma <bouma.luke@gmail.com>
2019-11-01 16:22:39 -0400

    add sector 8 reduction script

---
+++ drivers/tess_tuning_scripts/sector8_reduction.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+####################################
+# USAGE: ./sector8_reduction.sh & #
+####################################
+
+( source activate trex_37; source projid_1532.sh; wait ) & wait
+( source activate trex_37; source projid_1533.sh; wait ) & wait
+( source activate trex_37; source projid_1534.sh; wait ) & wait
+( source activate trex_37; source projid_1535.sh; wait ) & wait
+( source activate trex_37; source projid_1536.sh; wait ) & wait
+( source activate trex_37; source projid_1537.sh; wait ) & wait
+( source activate trex_37; source projid_1538.sh; wait ) & wait
+( source activate trex_37; source projid_1539.sh; wait ) & wait
+( source activate trex_37; source projid_1540.sh; wait ) & wait
+( source activate trex_37; source projid_1541.sh; wait ) & wait
+( source activate trex_37; source projid_1542.sh; wait ) & wait
+( source activate trex_37; source projid_1543.sh; wait ) & wait
+( source activate trex_37; source projid_1544.sh; wait ) & wait
+( source activate trex_37; source projid_1545.sh; wait ) & wait
+( source activate trex_37; source projid_1546.sh; wait ) & wait
+( source activate trex_37; source projid_1547.sh; wait ) & wait
