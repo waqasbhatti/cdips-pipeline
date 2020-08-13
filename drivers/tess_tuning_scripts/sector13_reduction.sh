0239dd04f72077b9328ebb281dd0783e4ce141cb
lgbouma <lgbouma@mit.edu>
2020-08-12 21:05:17 -0400

    minor

--- drivers/tess_tuning_scripts/sector13_reduction.sh
+++ drivers/tess_tuning_scripts/sector13_reduction.sh
@@ -7,9 +7,7 @@
 ( source activate trex_37; source projid_1612.sh; wait ) & wait
 ( source activate trex_37; source projid_1613.sh; wait ) & wait
 ( source activate trex_37; source projid_1614.sh; wait ) & wait
-( source activate trex_37; source projid_1615.sh; wait ) & wait
 ( source activate trex_37; source projid_1616.sh; wait ) & wait
-( source activate trex_37; source projid_1617.sh; wait ) & wait
 ( source activate trex_37; source projid_1618.sh; wait ) & wait
 ( source activate trex_37; source projid_1619.sh; wait ) & wait
 ( source activate trex_37; source projid_1620.sh; wait ) & wait
@@ -20,3 +18,7 @@
 ( source activate trex_37; source projid_1625.sh; wait ) & wait
 ( source activate trex_37; source projid_1626.sh; wait ) & wait
 ( source activate trex_37; source projid_1627.sh; wait ) & wait
+
+# NOTE: requires rest of cluster to be down
+( source activate trex_37; source projid_1617.sh; wait ) & wait
+( source activate trex_37; source projid_1615.sh; wait ) & wait
