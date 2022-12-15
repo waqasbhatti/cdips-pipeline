waqasbhatti/cdips-pipeline

commit 134a073c905ac74e58c64e05d8ec4c14343f9787

tessutils: drop 2048 default badframe quality flag
@jhartman

tessutils.py
@@ -411,7 +411,7 @@ def verify_badframe_move(fitslist, flagvalues, max_frac_badframes=0.25):
 
 
 def parallel_move_badframes(fitslist,
-                            flagvalues=[-1,4,32,36,2048,2080,2052,2084],
+                            flagvalues=[-1,4,32,36,2052,2080,2084],
                             nworkers=16,
                             maxworkertasks=1000):
     '''

commit 7e39f6fc7446072c985a25e2fb6cbeb729ddc56e

fix bugs; continued system path agnositicism
134a073c905ac74e58c64e05d8ec4c14343f9787 introduced a few typos, because
I wrote it when I was tired.

drivers/TESS_reduction.py
@@ -1868,7 +1868,7 @@ def main(fitsdir, fitsglob, projectid, field, camnum, ccdnum,
                                          nworkers=nworkers)
 
         # append CCD temperature information to headers
-        engdatadir = os.path.join(LOCAL_IMGBASE, 'ENGINEERING')
+        engdatadir = os.path.join(sv.LOCAL_IMGBASE, 'ENGINEERING')
         temperaturepklpath = os.path.join(
             engdatadir,
             'sector{:s}_ccd_temperature_timeseries.pickle'.

imagesubphot.py
@@ -2045,7 +2045,7 @@ def photometry_on_combined_photref(
                 return None
 
             origpath = os.path.join(
-                '/nfs/phtess2/ar0/TESS/FFI/BASE/reference-frames',
+                sv.LOCAL_IMGBASE, 'BASE', 'reference-frames',
                 os.path.basename(photref_sourcelist)
             )
             bar = read_object_reformed_catalog(

imageutils.py
@@ -1678,12 +1678,12 @@ def plot_stages_of_img_proc_sector_cam_ccd(
     if not os.path.exists(outdir):
         raise ValueError('did not find {}'.format(outdir))
 
-    datadir = (
-        '/nfs/phtess2/ar0/TESS/FFI/RED/sector-{}/cam{}_ccd{}/'.
-        format(sector, cam, ccd)
+    datadir = os.path.join(
+       sv.LOCAL_IMGBASE, 'RED',
+       'sector-{}/cam{}_ccd{}/'.format(sector, cam, ccd)
     )
-    diffdir = (
-        '/nfs/phtess2/ar0/TESS/FFI/RED_IMGSUB/FULL/s{}/RED_{}-{}-{}_ISP'.
+    diffdir = os.path.join(
+        sv.LOCAL_IMGBASE, 'RED_IMGSUB/FULL/s{}/RED_{}-{}-{}_ISP'.
         format(str(sector).zfill(4), cam, ccd, projid)
     )
 

lcutils.py
@@ -1737,7 +1737,8 @@ def apply_barycenter_time_correction(fitsilcfile):
             print('{}Z: ERR! got {} in TMID_BJD write to {}. mv -> graveyard.'.
                   format(datetime.utcnow().isoformat(), repr(e), fitsilcfile)
             )
-            graveyarddir = "/nfs/phtess1/ar1/TESS/FFI/GRAVEYARD"
+            graveyarddir = os.path.join(sv.LOCAL_IMGBASE, "GRAVEYARD")
+            if not os.path.exists(graveyarddir): os.mkdir(graveyarddir)
             shutil.move(fitsilcfile,
                         os.path.join(graveyarddir,
                                      os.path.basename(fitsilcfile)))

tessutils.py
@@ -134,15 +134,15 @@ if "Start TJD" not in badtime_df:
     # https://tess.mit.edu/public/files/orbit_times.csv to TESS julian date
     # (TJD).  This conversion is good to <0.5 seconds, based on a comparison
     # against https://archive.stsci.edu/missions/tess/doc/tess_drn/
-    from astroy.time import Time
-    _t0 = np.array(df['Start of Orbit'])
-    _t1 = np.array(df['End of Orbit'])
+    from astropy.time import Time
+    _t0 = np.array(badtime_df['Start of Orbit'])
+    _t1 = np.array(badtime_df['End of Orbit'])
     t0 = Time(_t0.astype(str), format='iso', scale='utc')
     t1 = Time(_t1.astype(str), format='iso', scale='utc')
     t0_tjd = t0.jd -  2457000 + (69.1817 / (24*60*60))
     t1_tjd = t1.jd -  2457000 + (69.1817 / (24*60*60))
-    df['Start TJD'] = t0_tjd
-    df['End TJD'] = t1_tjd
+    badtime_df['Start TJD'] = t0_tjd
+    badtime_df['End TJD'] = t1_tjd
 
 for orbit_ix in range(37, 91, 2):
     thistuple = (
@@ -930,8 +930,8 @@ def make_cluster_cutout_jpgs(sectornum, fitsdir, racenter, deccenter, field,
     """
 
     calimgdir = (
-        '/nfs/phtess2/ar0/TESS/FFI/RED/sector-{:d}/cam{:d}_ccd{:d}/'.
-        format(int(str(field[1:]).lstrip('0')), camera, ccd)
+        os.path.join(sv.LOCAL_IMGBASE, 'RED', 'sector-{:d}/cam{:d}_ccd{:d}/'.
+        format(int(str(field[1:]).lstrip('0')), camera, ccd))
     )
 
     calimgfiles = glob(os.path.join(calimgdir, 'tess*cal_img.fits'))
@@ -1450,7 +1450,7 @@ def explore_ccd_temperature_timeseries():
         Instrument Handbook, Appendix A.5 Engineering Data).
     """
 
-    engdatadir = '/nfs/phtess1/ar1/TESS/FFI/ENGINEERING/'
+    engdatadir = os.path.join(sv.LOCAL_IMGBASE, 'ENGINEERING')
     s01_first = 'tess2018331094053_sector01-eng.fits'
     s01_second = 'tess2018323111417_sector01-eng.fits'
     s02 = 'tess2018330083923_sector02-eng.fits'
@@ -1636,7 +1636,7 @@ def make_ccd_temperature_timeseries_pickle(sectornum):
         each of which contains a dictionary of time and temperature.
     """
 
-    engdatadir = '/nfs/phtess1/ar1/TESS/FFI/ENGINEERING/'
+    engdatadir = os.path.join(sv.LOCAL_IMGBASE, 'ENGINEERING')
     if sectornum==1:
         # identical to the other one on mast. LGB tested this in
         # explore_ccd_temperature_timeseries
@@ -2264,8 +2264,9 @@ def read_object_catalog(catalogfile):
 
 def merge_object_catalog_vs_cdips(
     in_reformed_cat_file, out_reformed_cat_file,
-    cdips_cat_file=('/nfs/phtess1/ar1/TESS/PROJ/lbouma/'
-                    'cdips_targets_v0.6_gaiasources_Rplt16_orclose.csv'),
+    cdips_cat_file=(os.path.join(
+        sv.CATPATH, 'cdips_targets_v0.6_gaiasources_Rplt16_orclose.csv')
+    ),
     G_Rp_cut=14):
     """
     the CDIPS project defines a cluster star sample for which we should make